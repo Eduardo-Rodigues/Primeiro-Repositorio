6f606ab7128329898b2b26e69c8afced90a23038
Eduardo Rodrigues <126730694+Eduardo-Rodigues@users.noreply.github.com>
2023-03-08 22:17:58 -0300

    Add files via upload

---
+++ PrimeiroPOO.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiropoo;
+
+/**
+ *
+ * @author eduro
+ */
+public class PrimeiroPOO {
+
+    public static void main(String[] args) { 
+        cachorro cachorro1 = new cachorro();
+        cachorro1.nome = ("Joao");
+        cachorro1.cor = ("Branco");
+        cachorro1.comportamento = ("Calmo");
+        cachorro1.sexo = ("Masculino");
+        cachorro1.classe = ("animal");
+        
+        cachorro cachorro2 = new cachorro();
+        cachorro2.nome = ("Jonas");
+        cachorro2.cor = ("Preto");
+        cachorro2.comportamento = ("Passivo");
+        cachorro2.sexo = ("Masculino");
+        cachorro2.classe = ("animal");
+        
+        cachorro cachorro3 = new cachorro();
+        cachorro3.nome = ("Psiu");
+        cachorro3.cor = ("Cinza");
+        cachorro3.sexo = ("Feminino");
+        cachorro3.classe = ("animal");
+        
+        cachorro cachorro4 = new cachorro();
+        cachorro4.nome = ("Faiado");
+        cachorro4.cor = ("Preto");
+        cachorro4.comportamento = ("Agressivo");
+        cachorro4.sexo = ("Masculino");
+        cachorro4.classe = ("animal");
+        
+        cachorro cachorro5 = new cachorro();
+        cachorro5.nome = ("Mimizento");
+        cachorro5.cor = ("Cinza e Preto");
+        cachorro5.comportamento = ("Agressivo");
+        cachorro5.sexo = ("Masculino");
+        cachorro5.classe = ("animal");}
+}
---
+++ cachorro.java
@@ -0,0 +1,19 @@
+package com.mycompany.primeiropoo;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eduro
+ */
+public class cachorro { 
+    String nome;
+    String cor;
+    String comportamento;
+    String sexo;
+    String classe;
+    
+}
